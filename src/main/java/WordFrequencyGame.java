5388113f944639a8e551e7e864a9cacba5661fed
Daniel Siu <daniel.siu@oocl.com>
2024-11-20 15:28:39 +0800

    refactor: remove unnecesary if else and set "1" to final string

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -3,30 +3,25 @@ import java.util.stream.Collectors;
 
 //todo
 // useless code, import, if else
-// stream instead of for loop
 // extract method in getResult
-// use constant for string e.g "\\s" regex string
 // temp field? inputList = list
-// remove unused import
 public class WordFrequencyGame {
 
     public static final String SPACE = "\\s+";
     public static final String LINE_BREAK = "\n";
     public static final String CALCULATE_ERROR = "Calculate Error";
+    public static final String ONE = " 1";
 
     public String getWordFrequency(String sentence) {
-        if (sentence.split(SPACE).length == 1) {
-            return sentence + " 1";
-        } else {
-            try {
-                //split the input string with 1 to n pieces of spaces
-                List<WordFrequency> wordFrequencyList = getInitialWordFrequencies(sentence);
-                //get the wordToWordfrequenciesMap for the next step of sizing the same word
-                List<WordFrequency> wordFrequencyListTemp = getWordFrequencies(wordFrequencyList);
-                return joinWordFrequencyResults(wordFrequencyListTemp);
-            } catch (Exception e) {
-                return CALCULATE_ERROR;
-            }
+        if (sentence.split(SPACE).length == 1) return sentence + ONE;
+        try {
+            //split the input string with 1 to n pieces of spaces
+            List<WordFrequency> wordFrequencyList = getInitialWordFrequencies(sentence);
+            //get the wordToWordfrequenciesMap for the next step of sizing the same word
+            List<WordFrequency> wordFrequencyListTemp = getWordFrequencies(wordFrequencyList);
+            return joinWordFrequencyResults(wordFrequencyListTemp);
+        } catch (Exception e) {
+            return CALCULATE_ERROR;
         }
     }
 
