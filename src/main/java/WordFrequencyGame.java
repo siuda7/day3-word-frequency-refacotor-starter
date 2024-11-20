adceb8f3d88b45bd0a90c3eff86ebef435d4b09f
Daniel Siu <daniel.siu@oocl.com>
2024-11-20 14:35:06 +0800

    refactor: rename variable and functions in WordFrequencyGame

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -21,28 +21,28 @@ public class WordFrequencyGame {
                     WordFrequency wordFrequency = new WordFrequency(word, 1);
                     wordFrequencyList.add(wordFrequency);
                 }
-                //get the map for the next step of sizing the same word
-                Map<String, List<WordFrequency>> map = getListMap(wordFrequencyList);
-                List<WordFrequency> list = new ArrayList<>();
-                for (Map.Entry<String, List<WordFrequency>> entry : map.entrySet()) {
-                    WordFrequency wordFrequency = new WordFrequency(entry.getKey(), entry.getValue().size());
-                    list.add(wordFrequency);
+                //get the wordFrequencyMap for the next step of sizing the same word
+                Map<String, List<WordFrequency>> wordFrequencyMap = getWordFrequencyMap(wordFrequencyList);
+                List<WordFrequency> wordFrequencyListTemp = new ArrayList<>();
+                for (Map.Entry<String, List<WordFrequency>> wordFrequencyEntry : wordFrequencyMap.entrySet()) {
+                    WordFrequency wordFrequency = new WordFrequency(wordFrequencyEntry.getKey(), wordFrequencyEntry.getValue().size());
+                    wordFrequencyListTemp.add(wordFrequency);
                 }
-                wordFrequencyList = list;
-                wordFrequencyList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-                StringJoiner joiner = new StringJoiner("\n");
-                for (WordFrequency w : wordFrequencyList) {
-                    String s = w.getWord() + " " + w.getWordCount();
-                    joiner.add(s);
+                wordFrequencyListTemp = wordFrequencyListTemp;
+                wordFrequencyListTemp.sort((word1, word2) -> word2.getWordCount() - word1.getWordCount());
+                StringJoiner wordFrequencyJoiner = new StringJoiner("\n");
+                for (WordFrequency wordFrequency : wordFrequencyListTemp) {
+                    String wordFrequencyString = wordFrequency.getWord() + " " + wordFrequency.getWordCount();
+                    wordFrequencyJoiner.add(wordFrequencyString);
                 }
-                return joiner.toString();
+                return wordFrequencyJoiner.toString();
             } catch (Exception e) {
                 return "Calculate Error";
             }
         }
     }
 
-    private Map<String, List<WordFrequency>> getListMap(List<WordFrequency> wordFrequencyList) {
+    private Map<String, List<WordFrequency>> getWordFrequencyMap(List<WordFrequency> wordFrequencyList) {
         Map<String, List<WordFrequency>> map = new HashMap<>();
         for (WordFrequency wordFrequency : wordFrequencyList) {
 //       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
