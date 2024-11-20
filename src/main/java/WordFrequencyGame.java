8aab8a0b3658069fe2668b9015379f227251399d
Daniel Siu <daniel.siu@oocl.com>
2024-11-20 14:17:32 +0800

    refactor: remove more empty spaces between variables

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -17,7 +17,6 @@ public class WordFrequencyGame {
             try {
                 //split the input string with 1 to n pieces of spaces
                 String[] arr = inputStr.split("\\s+");
-
                 List<Input> inputList = new ArrayList<>();
                 for (String s : arr) {
                     Input input = new Input(s, 1);
@@ -25,16 +24,13 @@ public class WordFrequencyGame {
                 }
                 //get the map for the next step of sizing the same word
                 Map<String, List<Input>> map = getListMap(inputList);
-
                 List<Input> list = new ArrayList<>();
                 for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
                     Input input = new Input(entry.getKey(), entry.getValue().size());
                     list.add(input);
                 }
                 inputList = list;
-
                 inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-
                 StringJoiner joiner = new StringJoiner("\n");
                 for (Input w : inputList) {
                     String s = w.getValue() + " " + w.getWordCount();
